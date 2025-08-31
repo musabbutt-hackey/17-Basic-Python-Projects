4aab73b61bef34c604fd5fb9ec065f86e833465f
Musab Butt <musabbutt9099@gmail.com>
2025-08-31 10:02:35 +0500

    Update Project 3(Rock,Paper,Sessior).py

--- Project 3(Rock,Paper,Sessior).py
+++ Project 3(Rock,Paper,Sessior).py
@@ -3,28 +3,28 @@ print("rock,paper,scissor")
 
 option = ["rock","paper","scissor"]
 
-computer = random.choice(option)
+friend = random.choice(option)
 
 user = input("Your choice: ")
 
-if user == computer:
+if user == friend:
     print("Its a draw!")
 
-elif user == "paper" and computer == "rock":
-    print(f"Computer choose: {computer} and you choose {user}\nYou won!") 
+elif user == "paper" and friend == "rock":
+    print(f"friend choose: {friend} and you choose {user}\nYou won!") 
 
-elif user == "paper" and computer == "scissor":
-    print(f"Computer choose: {computer} and you choose {user}\nYou lose!") 
+elif user == "paper" and friend == "scissor":
+    print(f"friend choose: {friend} and you choose {user}\nYou lose!") 
 
-elif user == "rock" and computer == "paper":
-    print(f"computer choose: {computer} and you choose: {user}\nYou lose!")
+elif user == "rock" and friend == "paper":
+    print(f"friend choose: {friend} and you choose: {user}\nYou lose!")
 
-elif user == "rock" and computer == "scissor":
-    print(f"computer choose: {computer} and you choose: {user}\nYou won!")
+elif user == "rock" and friend == "scissor":
+    print(f"friend choose: {friend} and you choose: {user}\nYou won!")
 
-elif user == "scissor" and computer == "paper":
-    print(f"computer choose: {computer} and you choose: {user}\nYou won!")
+elif user == "scissor" and friend == "paper":
+    print(f"friend choose: {friend} and you choose: {user}\nYou won!")
 
-elif user == "scissor" and computer == "rock":
-    print(f"computer choose: {computer} and you choose: {user}\nYou lose!")
+elif user == "scissor" and friend == "rock":
+    print(f"friend choose: {friend} and you choose: {user}\nYou lose!")
 
